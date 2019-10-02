e98269714a780d11a3a6ff430d07cd7ccf58b0ad
Justin Di Trani <jdt@bernal.sickkids.ca>
2019-10-02 18:30:35 -0400

    now with user run

--- Blntr4kGUI.py
+++ Blntr4kGUI.py
@@ -29,18 +29,24 @@ def timed():
     blottime = str(float(stime.value))
     arguments = ["python3","Bltrtimedrun.py","--btime",blottime]
     call(arguments)
+    button_user.disable()
     button_timed.disable()
+    button_align.disable()
     
 def userin():
     print("Power down")
-    arguments = ["python3","SIOpowerupdown.py","--updown","down"]
+    arguments = ["python3","Bltruserrun.py"]
     call(arguments)
     button_user.disable()
+    button_timed.disable()
+    button_align.disable()
     
 def align():
     print("Power down")
     arguments = ["python3","SIOpowerupdown.py","--updown","down"]
     call(arguments)
+    button_user.disable()
+    button_timed.disable()
     button_align.disable()
 
 app = App(title="Blottinator4000", layout="grid")
@@ -48,7 +54,7 @@ button_up   = PushButton(app, command=powerup,text="Ready", grid=[0,1])
 button_down = PushButton(app, command=powerdown, text="Abort", grid=[1,1])
 button_align= PushButton(app, command=align, text="Align the blotting pad using the XYZ stage", grid=[0,2])
 button_timed= PushButton(app, command=timed, text="Pre defined blotting time", grid=[0,3])
-button_user= PushButton(app, command=userin, text="Blot until user input", grid=[0,4])
+button_user = PushButton(app, command=userin, text="Blot until user input", grid=[0,4])
 
 stimelabel  = Text(app, text="Blot time (s)", grid=[1,3])
 stime       = TextBox(app, grid=[2,3], text="5")
---
+++ Bltruserrun.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 17:25:39 2019
+
+@author: jdt
+"""
+
+#import RPi.GPIO as GPIO
+import time, argparse
+import Blt8r4kpinlist as pin
+
+if __name__=='__main__':
+    
+    parser = argparse.ArgumentParser(description='No arguements for human blot')
+    parser.add_argument()
+    args = parser.parse_args()
+    
+    while True:
+        i = input("Press ender to start blotting")
+        if not i:
+            break
+        print("Your input:", i)
+    print("Blotting in 3 seconds")
+    
+    print('3')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    
+    while True:
+        #GPIO.output(pin.blotter,GPIO.HIGH)
+        i = input("Press ender to stop blotting")
+        if not i:
+            break
+        print("Your input:", i)
+    
+    #GPIO.output(pin.blotter,GPIO.LOW)
+    #GPIO.output(pin.plunger,GPIO.HIGH)
+    #time.sleep(1)
+    #GPIO.output(pin.plunger,GPIO.LOW)
